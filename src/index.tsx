80f3ebafb3d6ea97934da85087498480708c5153
Rohit Sikarwar <rohitsikarwar1998@gmail.com>
2021-06-29 13:35:06 +0530

    initial setup

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>Welcome!</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
